3f4b6a80304d973632bc375f3083e0f3f9ee8377
Reet Chahil <chahilr@outlook.com>
2022-08-11 22:55:21 -0400

    Fixed image loading

--- restaurant-page-project/src/index.js
+++ restaurant-page-project/src/index.js
@@ -1,1 +1,22 @@
-alert("This works!");
+import RestaurantImg from "./../images/restaurant.jpeg";
+import "./styles.css";
+
+function component() {
+  const header = document.createElement("div");
+  header.classList.add("header");
+
+  const headerImage = new Image();
+  headerImage.src = RestaurantImg;
+  headerImage.classList.add("header-img");
+  headerImage.alt = "Picture of restaurant";
+  headerImage.title = "Picture of restaurant";
+  headerImage.width = "500";
+  headerImage.id = "header-image";
+
+  header.appendChild(headerImage);
+
+  return header;
+}
+
+const content = document.querySelector("#content");
+content.appendChild(component());
--- restaurant-page-project/webpack.config.js
+++ restaurant-page-project/webpack.config.js
@@ -10,6 +10,21 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
   },
 };
