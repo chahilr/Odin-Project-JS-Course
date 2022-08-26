8f4271130b3186eb5b1f874e5782c079740c6b74
Reet Chahil <chahilr@outlook.com>
2022-08-25 23:57:17 -0400

    add list feature added and scrollbar styling added

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -1,6 +1,29 @@
 import "./styles.css";
 import addListImage from "../images/add-button.png";
 
+const listsWindow = document.querySelector(".lists-window");
+
 const addListBtn = document.querySelector("#addListBtn");
 addListBtn.src = addListImage;
 addListBtn.parentElement.style.border = "none";
+
+addListBtn.addEventListener("click", addList);
+
+function addList() {
+  const listContainer = document.createElement("div");
+  listContainer.classList.add("list-container");
+  listContainer.classList.add("scrollable");
+
+  const listTitle = document.createElement("h4");
+  listTitle.classList.add("list-title");
+  listTitle.innerText = "Temp";
+
+  const list = document.createElement("ul");
+  list.classList.add("list");
+
+  listContainer.appendChild(listTitle);
+  listContainer.appendChild(list);
+
+  listsWindow.appendChild(listContainer);
+  listsWindow.appendChild(addListBtn.parentElement);
+}
