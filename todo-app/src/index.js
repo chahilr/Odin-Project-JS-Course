7ceac3586f13f1a999af7dd1b05800b2bfcc4ab2
Reet Chahil <chahilr@outlook.com>
2022-08-26 20:26:32 -0400

    Check list items, Change list name, and responsive styling

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -7,16 +7,37 @@ const addListBtn = document.querySelector("#addListBtn");
 addListBtn.src = addListImage;
 addListBtn.parentElement.style.border = "none";
 
+const listItems = document.querySelectorAll(".list > li");
+listItems.forEach(addButton);
+
 addListBtn.addEventListener("click", addList);
+
+function addButton(listItem) {
+  const checkButton = document.createElement("input");
+  checkButton.type = "checkbox";
+  checkButton.style.width = "10px";
+  checkButton.style.height = "10px";
+  checkButton.style.backgroundColor = "white";
+  checkButton.style.borderRadius = "10px";
+
+  checkButton.onclick = () =>
+    (listItem.style.textDecoration = checkButton.checked
+      ? "line-through"
+      : "none");
+
+  listItem.prepend(checkButton);
+}
 
 function addList() {
   const listContainer = document.createElement("div");
   listContainer.classList.add("list-container");
   listContainer.classList.add("scrollable");
 
-  const listTitle = document.createElement("h4");
+  const listTitle = document.createElement("input");
+  listTitle.type = "text";
   listTitle.classList.add("list-title");
-  listTitle.innerText = "Temp";
+  listTitle.value = "Temp";
+  listTitle.setAttribute("autofocus", "autofocus");
 
   const list = document.createElement("ul");
   list.classList.add("list");
@@ -26,4 +47,5 @@ function addList() {
 
   listsWindow.appendChild(listContainer);
   listsWindow.appendChild(addListBtn.parentElement);
+  listTitle.focus();
 }
